10c7bcb9592ca7f84dae077e9362bb1dda025844
Eduardo Bacchi Kienetz <eduardo@kienetz.com>
2010-06-01 09:30:47 -0600

    Add /usr/local/include/wvstreams to the includes list
    That's needed when wvstreams is compiled from default configuration and
    command-line options (./configure ; make ; make install), because it
    ends up in /usr/local/include.

--- setup.py
+++ setup.py
@@ -6,7 +6,8 @@ WVSTREAMS="%s/wvstreams/include" % WVBUILD
 WVSTREAMSLIB="%s/%s-linux" % (WVBUILD,
     Popen(['uname', '-m'], stdout=PIPE).communicate()[0].rstrip())
 includes = [WVSTREAMS, "%s/include" % WVSTREAMSLIB,
-     "/usr/include/wvstreams", "/usr/include/xplc-0.3.13"]
+     "/usr/include/wvstreams", "/usr/include/xplc-0.3.13",
+     "/usr/local/include/wvstreams"]
 
 _wvcsv_mod = Extension('_wvcsv',
                         sources=['_wvcsvmodule.cc',
